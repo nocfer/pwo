bbdb8c360919af742e9a7cf894c9ad2d2836e345
Ferdinando Nocera <nocera.ferdinando@gmail.com>
2025-12-15 13:12:54 +0100

    feat: add expo-audio dependency and integrate audio playback in workout timer

--- hooks/session/index.ts
+++ hooks/session/index.ts
@@ -2,7 +2,5 @@
  * Session hooks barrel export
  */
 
-export { useSessionSteps, type Step } from "./useSessionSteps";
-export { useSessionTimer, type UseSessionTimerReturn } from "./useSessionTimer";
 export { useWorkoutSteps, type WorkoutStep } from "./useWorkoutSteps";
 export { useWorkoutTimer, type UseWorkoutTimerReturn } from "./useWorkoutTimer";
